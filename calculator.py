15066850797b36b056026fb1f37ca6a48ef86578
Rose Wambui <rose_wambui@outlook.com>
2025-01-20 22:13:50 +0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,22 @@
+operator = input("enter an operator ")
+num1 =float (input("Enter your first number "))
+num2 =float (input("Enter your second number "))
+
+if operator == "+":
+    result = num1 + num2
+    print(round(result, 3))
+elif operator == "-":
+    result = num1 - num2
+    print(round(result, 3))
+elif operator == "*":
+    result = num1 * num2
+    print(round(result, 3))
+elif operator == "/":
+    result = num1 / num2
+    print(round(result, 3))
+else:
+    print(f"{operator} is not a valid operator")            
+
+
+
+
